58ccfc606e40098e0642565c64f64f020ab2cdc7
shihongbing <piaolaidebing@126.com>
2021-04-19 20:48:12 +0800

    删除多余

--- src/pages/iam/cmiotAccount/service.ts
+++
@@ -1,6 +0,0 @@
-import { TableListParams } from '@/common/data/commondata';
-import http from '@/utils/http';
-
-export async function queryCmiotAccount(params?: TableListParams) {
-  return http.json('/api/iam/cmiotacc/page', params);
-}
